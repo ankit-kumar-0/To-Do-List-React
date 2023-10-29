ea2ec0770ab4bb9e233dc2169498c1c0fcac54c0
Ankit <ankitrajpsr@gmail.com>
2023-10-29 15:17:56 +0530

    Added Edit functionality

--- src/Todo.js
+++ src/Todo.js
@@ -4,20 +4,45 @@ import todo from "../src/img/icon.png";
 function Todo() {
   const [task, setTask] = useState("");
   const [listData, setListData] = useState([]);
+  const [toggleBtn, setToggleBtn] = useState(true);
+  const [isEditTask, setIsEditTask] = useState(null);
 
   function addTask() {
     if (!task) {
+      alert("Please Add your task");
+    } else if (task && !toggleBtn) {
+      setListData(
+        listData.map((data) => {
+          if (data.id === isEditTask) {
+            return { ...data, name: task };
+          }
+          return data;
+        })
+      );
+      setToggleBtn(true);
+      setTask("");
+      setIsEditTask(null);
     } else {
-      setListData([...listData, task]);
+      const allTask = { id: new Date().getTime().toString(), name: task };
+      setListData([...listData, allTask]);
       setTask("");
     }
   }
 
-  function removeTask(id) {
-    const updatedListData = listData.filter((elem, ind) => {
-      return ind !== id;
+  function removeTask(ind) {
+    const updatedListData = listData.filter((data) => {
+      return ind !== data.id;
     });
     setListData(updatedListData);
+  }
+
+  function editTask(id) {
+    let newEditTask = listData.find((data) => {
+      return data.id === id;
+    });
+    setToggleBtn(false);
+    setTask(newEditTask.name);
+    setIsEditTask(id);
   }
 
   function removeAll() {
@@ -39,18 +64,27 @@ function Todo() {
               value={task}
               onChange={(e) => setTask(e.target.value)}
             />
-            <button onClick={addTask}>Add</button>
+            {toggleBtn ? (
+              <button onClick={addTask}>Add</button>
+            ) : (
+              <button onClick={addTask}>Edit</button>
+            )}
           </div>
-          {listData.map((data, ind) => {
+          {listData.map((data) => {
             return (
               <>
-                <p key={ind}>
+                <p key={data.id}>
                   <div className="listData">
-                    {data}
+                    {data.name}
+                    <i
+                      class="bx bx-edit"
+                      id="span1"
+                      onClick={() => editTask(data.id)}
+                    ></i>
                     <i
                       className="bx bx-x"
                       id="span"
-                      onClick={() => removeTask(ind)}
+                      onClick={() => removeTask(data.id)}
                     ></i>
                   </div>
                 </p>
